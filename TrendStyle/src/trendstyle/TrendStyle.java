28c001ababd0adbfca6cbe7bf2c86fc92143b622
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-05-23 10:36:11 -0700

    Structure Page

--- TrendStyle/src/trendstyle/TrendStyle.java
+++ TrendStyle/src/trendstyle/TrendStyle.java
@@ -4,6 +4,8 @@
  */
 package trendstyle;
 
+import views.SplashScreen;
+
 /**
  *
  * @author Unknown Account
@@ -14,7 +16,8 @@ public class TrendStyle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SplashScreen page = new SplashScreen();
+        page.setVisible(true);
     }
     
 }
