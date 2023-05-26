35ec8914869d5446e385e9fe10554ffa579d6f70
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-05-25 18:38:41 -0700

    Update communication.java

--- TrendStyle/src/communication/communication.java
+++ TrendStyle/src/communication/communication.java
@@ -38,9 +38,7 @@ public class communication {
     public boolean registerClient(String username, String password, String cpf, String nome, String sobrenome, String email, String telefone, String cep, String rua, String numero, String cidade, String estado) {
         mysql.conectaBanco();
 
-        String consulta = "CALL registerClient('" + username + "', '" + password + "', '" + cpf + "', '" + nome + "', '" + sobrenome + "', '" +
-                          email + "', '" + telefone + "', '" + cep + "', '" + rua + "', '" + numero + "', '" +
-                          cidade + "', '" + estado + "')";
+        String consulta = "CALL registerClient('" + username + "', '" + password + "', '" + cpf + "', '" + nome + "', '" + sobrenome + "', '" + email + "', '" + telefone + "', '" + cep + "', '" + rua + "', '" + numero + "', '" + cidade + "', '" + estado + "')";
 
         mysql.executarSQL(consulta);
 
