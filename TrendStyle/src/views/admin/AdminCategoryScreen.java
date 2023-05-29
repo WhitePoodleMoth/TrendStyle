e31b1839d7c346d03b8cf055381090acc38d7f66
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-05-29 14:58:58 -0700

    Admin Category Structure

--- TrendStyle/src/views/admin/AdminCategoryScreen.java
+++ TrendStyle/src/views/admin/AdminCategoryScreen.java
@@ -4,6 +4,8 @@
  */
 package views.admin;
 
+import java.awt.Toolkit;
+
 /**
  *
  * @author Unknown Account
@@ -16,6 +18,8 @@ public class AdminCategoryScreen extends javax.swing.JFrame {
      */
     public AdminCategoryScreen(int _ID) {
         initComponents();
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("../../media/TrendStyleIcon.png")));
+        setLocationRelativeTo(null);
         this.ID = _ID;
     }
 
@@ -28,21 +32,50 @@ public class AdminCategoryScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Panel = new javax.swing.JPanel();
+        ButtonBack = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("TrendStyle - Admin Category");
+
+        Panel.setBackground(new java.awt.Color(255, 255, 255));
+        Panel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        ButtonBack.setBorderPainted(false);
+        ButtonBack.setContentAreaFilled(false);
+        ButtonBack.setMaximumSize(new java.awt.Dimension(50, 50));
+        ButtonBack.setMinimumSize(new java.awt.Dimension(50, 50));
+        ButtonBack.setPreferredSize(new java.awt.Dimension(50, 50));
+        ButtonBack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButtonBackActionPerformed(evt);
+            }
+        });
+        Panel.add(ButtonBack, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 120, 50));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(Panel, javax.swing.GroupLayout.PREFERRED_SIZE, 960, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(Panel, javax.swing.GroupLayout.PREFERRED_SIZE, 540, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ButtonBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonBackActionPerformed
+        AdminPanel page = new AdminPanel(this.ID);
+        page.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_ButtonBackActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,5 +113,7 @@ public class AdminCategoryScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ButtonBack;
+    private javax.swing.JPanel Panel;
     // End of variables declaration//GEN-END:variables
 }
